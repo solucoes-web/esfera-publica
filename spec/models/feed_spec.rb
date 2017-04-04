794ad56705c4e5791097e996db028502ed43d6f1
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-04-04 12:19:07 -0300

    testes unitarios que faltaram das basic-infos

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -16,4 +16,29 @@ RSpec.describe Feed, type: :model do
     feed.url = "http://example.org"
     expect(feed).to be_invalid
   end
+
+  it "gets basic info from rss" do
+    feed = build(:feed, url: "http://feeds.folha.uol.com.br/folha/emcimadahora/rss091.xml")
+    VCR.use_cassette "folha" do
+      feed.get_basic_info
+      expect(feed.name).not_to be_nil
+      expect(feed.favicon).not_to be_nil
+    end
+  end
+
+  it "gets basic info from host" do
+    feed = build(:feed, url: "https://www.propublica.org/")
+    VCR.use_cassette "propublica" do
+      feed.get_basic_info
+      expect(feed.name).not_to be_nil
+      expect(feed.favicon).not_to be_nil
+    end
+  end
+
+  it "raises exception in invalid url" do
+    feed = build(:feed, url: "http://example.com")
+    VCR.use_cassette "example" do
+      expect{ feed.get_basic_info }.to raise_error("RSS inválido")
+    end
+  end
 end
