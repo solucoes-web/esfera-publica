960547b61900dae05dfb655284090791cbed143e
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-04-18 00:00:25 -0300

    Revert "Extrai palavras-chaves"

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :feed
 
-  acts_as_taggable_on :keywords
-
   scope :tagged_with, ->(tag) {
        joins("INNER JOIN taggings ON taggings.taggable_id = items.feed_id\
               INNER JOIN tags ON tags.id = taggings.tag_id AND\
@@ -22,29 +20,13 @@ class Item < ApplicationRecord
   }
 
   before_save do
-    get_keywords
     begin
       html = open(url).read
-      get_images(html)
-      get_content(html)
+    #  self.image ||= (/property="og:image" content="(.+)"/.match html)[1]
+      self.image ||= OpenGraph.new(html).images.first
+      self.content = Readability::Document.new(html).content
     rescue Exception => e
       logger.info e.message
     end
-  end
-
-  private
-  def get_keywords
-    keywords = OTS.parse(summary, language: "pt").keywords
-    self.keyword_list = (keywords - Stopwords.first).join(', ')
-    # provavelmente vou querer isso em outras linguas no futuro
-  end
-
-  def get_images(html)
-    #  self.image ||= (/property="og:image" content="(.+)"/.match html)[1]
-    self.image ||= OpenGraph.new(html).images.first
-  end
-
-  def get_content(html)
-    self.content = Readability::Document.new(html).content
   end
 end
