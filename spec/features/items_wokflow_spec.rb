65d69fd4dc48824b37eda2b9e759a35b49ff1a2b
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-04-17 23:37:43 -0300

    micro refatoração dos testes dos itens

---
+++ spec/features/items_wokflow_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "ItemWorkflows", type: :feature do
+  before :each do
+    clean_db
+    (@feed = build(:feed, url: "http://example.org")).save(validate: false)
+    @item = create(:item, feed: @feed, name: "title")
+  end
+
+  scenario "list items" do
+    visit items_path
+
+    expect(page).to have_content(@item.name)
+  end
+
+  scenario "list feed items" do
+    visit items_path @feed.id
+    expect(page).to have_content(@item.name)
+  end
+
+  scenario "show item" do
+    visit items_path @feed.id
+    click_link "Ler aqui"
+
+    expect(page).to have_css('.modal')
+  end
+end
