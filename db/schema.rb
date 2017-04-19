7330760674cf8e3011ad54cd4d632504a1b95390
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-04-19 16:50:29 -0300

    corrigindo o bug que quebrou o Travis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417212952) do
+ActiveRecord::Schema.define(version: 20170419003014) do
 
   create_table "feeds", force: :cascade do |t|
     t.string   "name"
