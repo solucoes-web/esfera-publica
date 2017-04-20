b934fe8b4a57731abe38d5676fdb81d2396c1f11
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-04-20 11:55:48 -0300

    inclui a manchete nos palavras-chave e ajustei o peso: as palavras da manchete são 2 vezes mais importantes que as do resumo que são 2 vezes mais importantes que as do conteudo

--- app/models/item.rb
+++ app/models/item.rb
@@ -34,7 +34,8 @@ class Item < ApplicationRecord
 
   private
   def get_keywords
-    text = (summary || "") + (content || "")
+    # ajustei os pesos: 4 vezes para a manchete, 2 para o resumo e 1 para o conteudo
+    text = (Array.new(4, name) + Array.new(2, summary) + [content]).join(' ')
     text = ActionController::Base.helpers.sanitize(text)
     keywords = OTS.parse(text, language: "pt").keywords[0..4]
     self.keyword_list = (keywords - Stopwords.first).join(', ')
